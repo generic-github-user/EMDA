00e0b7e8d1ffe589b7924784e445b38786c4b14a
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-07 13:30:27 -0400

    read lines from file

--- emda_parser.py
+++ emda_parser.py
@@ -6,3 +6,14 @@
 
 from IPython.display import JSON
 import json
+
+
+# In[18]:
+
+
+# path = 'entities.edma'
+# path = '../todo_july.txt'
+path = './readme_source.txt'
+with open(path, 'r') as file:
+    content = file.read()
+lines = content.splitlines()
